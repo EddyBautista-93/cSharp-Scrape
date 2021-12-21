7b7b530b8810c0e4240627207906d8590b2d63a0
EddyBautista <bautista.j.eddy@gmail.com>
2021-12-20 19:00:06 -0600

    import packages;

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+namespace cSharp_Scrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
