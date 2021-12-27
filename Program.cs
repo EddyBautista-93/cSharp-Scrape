490085e8b30c542f8c12594ab928e7ff4d50b741
EddyBautista <bautista.j.eddy@gmail.com>
2021-12-27 09:11:15 -0600

    grab text we want from the page and notes

--- Program.cs
+++ Program.cs
@@ -4,10 +4,16 @@ using HtmlAgilityPack;
 using System.IO;
 using System.Collections.Generic;
 using System.Globalization;
+/*
+    Packages used 
+    HtmlAgilityPack - html parser to read/write DOM
+    CsvHelper - read/write CSV files
+*/
 namespace cSharp_Scrape
 {
     class Program
     {
+        // Used for table of content titles 
         public class Row
         {
             public string Title { get; set; }
@@ -15,9 +21,13 @@ namespace cSharp_Scrape
         }
         static void Main(string[] args)
         {
+            // htmlweb is a utility class to get html docs from http
             HtmlWeb web = new HtmlWeb();
 
+            // htmlDocument represents a complete html document
             HtmlDocument doc = web.Load("https://en.wikipedia.org/wiki/Greece");
-        }
+
+            // parse and select the nodes containing span.toctext
+            var HeaderNames = doc.DocumentNode.SelectNodes("//span[@class='toctext']");        }
     }
 }
